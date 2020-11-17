70d74f8639ca7c7de5df054cdcc257950e7e6711
shhong94 <shhong94@naver.com>
2020-11-17 17:39:35 +0900

    11.17

---
+++ 3rdTeamProject/src/main/java/com/sist/web/DiaryContoller.java
@@ -0,0 +1,13 @@
+package com.sist.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DiaryContoller {
+	
+	@RequestMapping("diary/list.do")
+	public String diary_list(){
+		return "list";
+	}
+}
