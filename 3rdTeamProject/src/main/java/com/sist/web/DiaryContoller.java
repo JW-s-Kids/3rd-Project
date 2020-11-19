a5950b0d72cde254a7c102891baa4eea59f92a9b
shhong94 <shhong94@naver.com>
2020-11-19 17:11:08 +0900

    11.19

--- 3rdTeamProject/src/main/java/com/sist/web/DiaryContoller.java
+++ 3rdTeamProject/src/main/java/com/sist/web/DiaryContoller.java
@@ -6,8 +6,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DiaryContoller {
 	
+	
+	// 여행기 리스트 ==================================================================================================================================================
 	@RequestMapping("diary/list.do")
 	public String diary_list(){
 		return "diary/list";
+	}
+	
+	
+	@RequestMapping("diary/detail.do")
+	public String diary_detail(){
+		return "diary/detail";
 	}
 }
