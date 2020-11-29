a39785153702673995df07df4bce90011d6ae8a8
shhong94 <63003560+shhong94@naver.com>
2020-11-30 01:01:29 +0900

    11.30

--- 3rdTeamProject/src/main/java/com/sist/web/DiaryContoller.java
+++ 3rdTeamProject/src/main/java/com/sist/web/DiaryContoller.java
@@ -1,18 +1,27 @@
 package com.sist.web;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sist.dao.DiaryDAO;
 import com.sist.dao.DiaryVO;
 import com.sist.dao.Diary_replyVO;
 import com.sist.dao.Diary_scrapVO;
+
 import com.sist.service.NaverManager;
 
+import java.io.*;
+import java.io.File;
 import java.util.*;
+import java.text.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -466,4 +475,127 @@ public class DiaryContoller {
 //			return "../jobKnowledge/box.jsp";
 	}
 	
+	
+	
+	
+	/*// 여행기 사진 업로드 (싱글) ========================================================================================================
+	@RequestMapping("diary/singleImageuploader.do")
+	public String duary_upload_file(SmarteditorVO vo, HttpServletRequest request){
+		
+		String callback = vo.getCallback();
+		String callback_func = vo.getCallback_func();
+		String file_result = "";
+		String result = "";
+		MultipartFile multiFile = vo.getFiledata();
+		
+		try {
+			if(multiFile != null && multiFile.getSize() > 0 && StringUtils.isNotBlank(multiFile.getName())){
+				if(multiFile.getContentType().toLowerCase().startsWith("image/")){
+					String oriName = multiFile.getName();
+					String uploadPath = request.getRealPath("/img");
+					String path = uploadPath + "/smarteditor/";
+					File file = new File(path);
+					
+					if(!file.exists()){
+						file.mkdirs();
+					}
+					
+					String fileName = UUID.randomUUID().toString();
+					vo.getFiledata().transferTo(new File(path + fileName));
+					file_result = "&bNewLine=true&sFileName=" + oriName + "&sFileURL=/img/smarteditor" + fileName;
+				
+				}
+				else{
+					file_result += "&errstr=error";
+				}
+			}
+			else{
+				file_result += "&errstr=error";
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	
+
+		
+		
+		 return "";
+	}*/
+	
+	
+	
+	// 여행기 사진 업로드 (멀티) ========================================================================================================
+	@RequestMapping("/file_uploader_html5.do")
+	public void diary_file_uploader_html5(HttpServletRequest request, HttpServletResponse response){
+		try { 
+			//파일정보 
+			String sFileInfo = ""; 
+			//파일명을 받는다 - 일반 원본파일명 
+			String filename = request.getHeader("file-name"); 
+			//파일 확장자 
+			String filename_ext = filename.substring(filename.lastIndexOf(".")+1); 
+			//확장자를소문자로 변경 
+			filename_ext = filename_ext.toLowerCase(); 
+			//이미지 검증 배열변수 
+			String[] allow_file = {"jpg","png","bmp","gif"}; 
+			//돌리면서 확장자가 이미지인지 
+			int cnt = 0; 
+			for(int i=0; i<allow_file.length; i++) { 
+				if(filename_ext.equals(allow_file[i])){
+					cnt++; 
+				} 
+			} 
+			//이미지가 아님 
+			if(cnt == 0) { 
+				PrintWriter print = response.getWriter(); 
+				print.print("NOTALLOW_"+filename); 
+				print.flush(); 
+				print.close(); 
+			} 
+			else { 
+				//이미지이므로 신규 파일로 디렉토리 설정 및 업로드 
+				//파일 기본경로 
+				String dftFilePath = request.getSession().getServletContext().getRealPath("/"); 
+				//파일 기본경로 _ 상세경로 
+				String filePath = dftFilePath + "resources" + File.separator + "editor" + File.separator +"multiupload" + File.separator; 
+				File file = new File(filePath); 
+				if(!file.exists()) { 
+					file.mkdirs(); 
+				} 
+				String realFileNm = ""; 
+				SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss"); 
+				String today= formatter.format(new java.util.Date()); 
+				realFileNm = today+UUID.randomUUID().toString() + filename.substring(filename.lastIndexOf(".")); 
+				String rlFileNm = filePath + realFileNm; 
+				
+				///////////////// 서버에 파일쓰기 ///////////////// 
+				InputStream is = request.getInputStream(); 
+				OutputStream os=new FileOutputStream(rlFileNm); 
+				int numRead; 
+				byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))]; 
+				while((numRead = is.read(b,0,b.length)) != -1){
+					os.write(b,0,numRead); 
+				} 
+				if(is != null) {
+					is.close(); 
+				} 
+				os.flush(); 
+				os.close(); 
+				
+				///////////////// 서버에 파일쓰기 ///////////////// 
+				// 정보 출력 
+				sFileInfo += "&bNewLine=true"; 
+				// img 태그의 title 속성을 원본파일명으로 적용시켜주기 위함 
+				sFileInfo += "&sFileName="+ filename;; 
+				sFileInfo += "&sFileURL="+"/resources/editor/multiupload/"+realFileNm; 
+				PrintWriter print = response.getWriter(); 
+				print.print(sFileInfo); 
+				print.flush(); print.close(); 
+				} 
+			} catch (Exception e) {
+				e.printStackTrace(); 
+			}
 }
+}	
+
+
--- 3rdTeamProject/src/main/webapp/WEB-INF/main/header.jsp
+++ 3rdTeamProject/src/main/webapp/WEB-INF/main/header.jsp
@@ -5,7 +5,7 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>오늘 뭐하니?</title>
 <link rel="stylesheet" type="text/css" href="../css/member/member.css?after">
 
 </head>
